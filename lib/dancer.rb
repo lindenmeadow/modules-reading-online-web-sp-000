102f20649111d071bbdaee4d647a43b04c2093c7
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-07-05 20:11:08 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,7 +2,7 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
 class Dancer
-  
+
   include Dance
   extend MetaDancing
 
@@ -11,5 +11,5 @@ class Dancer
   def initialize(name)
     @name = name
   end
-  
+
 end
